d56c51a4972755ea011ced8cdda6a3089ba39c9d
Etienne <etienne.gautier@me.com>
2017-08-16 20:44:52 +0200

    Removed "taw" element.

--- removeAd.js
+++ removeAd.js
@@ -2,6 +2,3 @@
 if (document.contains(document.getElementById("tads"))) {
   document.getElementById("tads").remove();
 }
-if (document.contains(document.getElementById("taw"))) {
-  document.getElementById("taw").remove();
-}
