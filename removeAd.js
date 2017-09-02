d87212d91889bab64852c070a96f7f9bbdd387c8
etienne <etienne.gautier@aol.fr>
2017-09-02 19:34:08 +0200

    Duckduckgo support.

--- removeAd.js
+++ removeAd.js
@@ -1,4 +1,12 @@
-
-if (document.contains(document.getElementById("tads"))) {
-  document.getElementById("tads").remove();
-}
+  if ("duckduckgo.com" === window.location.hostname && document.contains(document.getElementById("ads"))) {
+    let ads = document.getElementById("ads");
+    // prevent ajax calls
+    let watch = setInterval(() => {
+      if (!ads.classList.contains('is-hidden')) {
+        ads.remove();
+        clearInterval(watch);
+      }
+    }, 50);
+  } else if (document.contains(document.getElementById("tads"))) {
+    document.getElementById("tads").remove();
+  }
