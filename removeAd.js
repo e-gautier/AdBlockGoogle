709959274c3ea908eb4eb8f4b52bc20d6dd2336d
Etienne GAUTIER <etienne.gautier@outlook.com>
2020-03-30 16:06:37 +0200

    removed DDG support

--- removeAd.js
+++ removeAd.js
@@ -1,12 +1,3 @@
-  if ("duckduckgo.com" === window.location.hostname && document.contains(document.getElementById("ads"))) {
-    let ads = document.getElementById("ads");
-    // prevent ajax calls
-    let watch = setInterval(() => {
-      if (!ads.classList.contains('is-hidden')) {
-        ads.remove();
-        clearInterval(watch);
-      }
-    }, 50);
-  } else if (document.contains(document.getElementById("tads"))) {
-    document.getElementById("tads").remove();
-  }
+if (document.contains(document.getElementById("tads"))) {
+  document.getElementById("tads").remove();
+}
